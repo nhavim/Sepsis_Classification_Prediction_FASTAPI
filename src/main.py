1e46917c948ebb93685eebd7d0ff2300f39bab76
Nathaniel Havim <nhavim123@gmail.com>
2023-06-10 20:21:15 +0000

    Error corrections

--- src/main.py
+++ src/main.py
@@ -75,4 +75,5 @@ def predict_sepsis_endpoit(PRG: float, PL: float, PR: float, SK: float, TS: floa
     
     return result
 
-if_name_=="__main__":uvicorn.run(app, host="0.0.0.0", port=8080, reload=True)
+if __name__ == "__main__":
+    uvicorn.run(app,host="0.0.0.0", port=8080, reload=True)
